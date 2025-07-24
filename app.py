fe2c93dff4b0355ec374fd54d1b5ae30daecbceb
ashpynb <ash.machado@estudante.ufjf.br>
2025-07-24 19:06:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ def opcoes_menu():  #feito
     st.title(":orange[Fórmula] :orange[Econômica]")
     st.header('_Os melhores simuladores e cálculos econômicos estão aqui!_', divider = 'orange')
 
+
+    #vou criar uma barra lateral para entrarem em contato, informações complementares e deixar o site bonito!
+    with st.sidebar:
+        st.markdown("## 🍊 Bem-vindo!")
+        st.markdown(":orange[Este é seu painel lateral com estilo laranja.]")
+        st.markdown('## ℹ️ Sobre')
+        st.write("Simulador Econômico em Python - UFJF-GV")
+        st.markdown("### 📬 Entre em contato:")
+        st.link_button('Contato com desenvolvedor',"https://wa.me/5533998488760?text=Ol%C3%A1%21%20Vim%20pelo%20seu%20site%20F%C3%B3rmula%20Econ%C3%B4mica.%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%21")
+        st.write("📧 Email: ash.machado@estudante.ufjf.br")
+        st.markdown("---")
+        st.caption("Versão 1.0 • 2025")
     opcao = st.selectbox("Escolha um Simulador:", 
                          ["Macroeconômico", "Microeconômico", "Financeiro", "Estatístico", "Conversor de Moeda"])
 
@@ -27,8 +39,7 @@ def opcoes_menu():  #feito
         elif opcao == "Conversor de Moeda":
             st.session_state.tela = 'conv'
     st.write('Feito por Ash Machado')
-    st.link_button('Contato com desenvolvedor',"https://wa.me/5533998488760?text=Ol%C3%A1%21%20Vim%20pelo%20seu%20site%20F%C3%B3rmula%20Econ%C3%B4mica.%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%21")
-    #Novo
+    
 
 #menu macroeconômico
 def tela_macro(): #feito
