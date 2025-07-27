988a6993fc83e81514c606720aefb9b58c9f8fd6
ashpynb <ash.machado@estudante.ufjf.br>
2025-07-27 19:35:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -293,66 +293,63 @@ def tela_est():
         st.markdown("### Resultados:")
         if "Somatório" in opcoes:
             with st.expander("Somatórios:"):
-                st.write(f"**Somatório X:** {col_x.sum():.2f}")
-                st.write(f"**Somatório X²:** {(col_x**2).sum():.2f}")
+                st.metric(f"**Somatório X:** {col_x.sum():.2f}")
+                st.metric(f"**Somatório X²:** {(col_x**2).sum():.2f}")
                 if not col_y.empty:
-                    st.write(f"**Somatório Y:** {col_y.sum():.2f}")
-                    st.write(f"**Somatório Y²:** {(col_y**2).sum():.2f}")
-                    st.write(f"**Somatório XY:** {(col_y * col_x).sum():.2f}")
+                    st.metric(f"**Somatório Y:** {col_y.sum():.2f}")
+                    st.metric(f"**Somatório Y²:** {(col_y**2).sum():.2f}")
+                    st.metric(f"**Somatório XY:** {(col_y * col_x).sum():.2f}")
         if "Produtório" in opcoes:
             with st.expander("Produtórios:"):
-                st.write(f"**Produtório X:** {math.prod(col_x):.2f}")
-                st.write(f"**Produtório X²:** {math.prod(col_x**2):.2f}")
+                st.metric(f"**Produtório X:** {math.prod(col_x):.2f}")
+                st.metric(f"**Produtório X²:** {math.prod(col_x**2):.2f}")
                 if not col_y.empty:
-                    st.write(f"**Produtório Y:** {math.prod(col_y):.2f}")
-                    st.write(f"**Produtório Y²:** {math.prod(col_y**2):.2f}")
-                    st.write(f"**Produtório de XY:** {math.prod(col_x * col_y):.2f}")
+                    st.metric(f"**Produtório Y:** {math.prod(col_y):.2f}")
+                    st.metric(f"**Produtório Y²:** {math.prod(col_y**2):.2f}")
+                    st.metric(f"**Produtório de XY:** {math.prod(col_x * col_y):.2f}")
         if "Média Aritmética" in opcoes:
             with st.expander("Médias Aritméticas:"):
-                n_x = (len(col_x))
-                st.write(f"**Média Aritmética de X:** {col_x.mean():.2f}")
+                st.metric(f"**Média Aritmética de X:** {col_x.mean():.2f}")
                 if not col_y.empty:
-                    n_y = (len(col_y))
-                    st.write(f"**Média Aritmédica de Y:** {col_y.mean():.2f}")
-                    st.write(f"**Média Aritmédica de X + Y:** {((col_y.sum() + col_x.sum())/(n_y + n_x)):.2f}")
+                    st.metric(f"**Média Aritmédica de Y:** {col_y.mean():.2f}")
+                    st.metric(f"**Média Aritmédica de X + Y:** {((col_y.sum() + col_x.sum())/(n_y + n_x)):.2f}")
         if "Média Ponderada" in opcoes:
             with st.expander('Média Ponderada'):
                 if col_y.empty:
                     st.error("Utiliza-se X como valor e Y como peso")
                 if not col_y.empty:
-                    st.write(f"**Média Ponderada de X com peso Y:** {(((col_y * col_x).sum())/(col_y).sum()):.2f}")
+                    st.metric(f"**Média Ponderada de X com peso Y:** {(((col_y * col_x).sum())/(col_y).sum()):.2f}")
         if "Mediana" in opcoes:
             with st.expander("Medianas"):
-                st.write(f"**Mediana X:** {col_x.median():.2f}")
+                st.metric(f"**Mediana X:** {col_x.median():.2f}")
                 if not col_y.empty:
-                    st.write(f"**Mediana Y:** {col_y.median():.2f}")
+                    st.metric(f"**Mediana Y:** {col_y.median():.2f}")
         if "Moda" in opcoes:
             with st.expander("Modas"):
                 # CORREÇÃO: Converte a série de modas para uma lista e depois para uma string  #ajuda do chatgpt pq estava retornando lista
                 modax = col_x.mode()
                 if not modax.empty:
                     modax_str = ', '.join([str(v) for v in modax])
-                    st.write(f"**Moda de X:** {modax_str}")
+                    st.metric(f"**Moda de X:** {modax_str}")
                 if not col_y.empty:
                     moday = col_y.mode()
                     if not moday.empty:
                         moday_str = ', '.join([str(v) for v in moday])
-                        st.write(f"**Moda de Y:** {moday_str}")
-
+                        st.metric(f"**Moda de Y:** {moday_str}")
         if "Amplitude" in opcoes:
             with st.expander("Amplitude"):
                 amplitudex = col_x.max() - col_x.min()
-                st.write(f"**Amplitude de X: {amplitudex:.2f}")
+                st.metric(f"**Amplitude de X: {amplitudex:.2f}")
                 if not col_y.empty:
                     amplitudey = col_y.max() - col_y.min()
-                    st.write(f"**Amplitude de Y:** {amplitudey:.2f}")
+                    st.metric(f"**Amplitude de Y:** {amplitudey:.2f}")
         if "Variância" in opcoes:
              with st.expander("Variâncias"):
-                st.write(f"**Variância Amostral de X:** {col_x.var():.2f}")
-                st.write(f"**Variância Populacional de X:** {col_x.var(ddof=0):.2f}")
+                st.metric(f"**Variância Amostral de X:** {col_x.var():.2f}")
+                st.metric(f"**Variância Populacional de X:** {col_x.var(ddof=0):.2f}")
                 if not col_y.empty:
-                    st.write(f"**Variância Amostral de Y** {col_y.var():.2f}")
-                    st.write(f"**Variância Populacional de Y** {col_y.var(ddof=0):.2f}")
+                    st.metric(f"**Variância Amostral de Y** {col_y.var():.2f}")
+                    st.metric(f"**Variância Populacional de Y** {col_y.var(ddof=0):.2f}")
         if 'Desvio Padrão' in opcoes:
             with st.expander("Desvio Padrão:"):
                 st.metric("Desvio Padrão Amostral de X", f"{col_x.std():.2f}")
@@ -484,3 +481,4 @@ def main():
 #loop 
 if __name__ == "__main__":
     main()
+    
