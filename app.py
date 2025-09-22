62359c480b32d5c9c011822233c9efe74167f6f7
ashpynb <ash.machado@estudante.ufjf.br>
2025-09-21 22:22:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np 
 import math
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title="Fórmula Econômica",
