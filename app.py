16a66ffbc42811cdd365837754d6c895fccc9b04
ashpynb <ash.machado@estudante.ufjf.br>
2025-09-23 20:50:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -370,7 +370,7 @@ def tela_micro():  #EM DESENVOLVIMENTO --- pretendo integrar MATPLOTLIB para sim
                                 case "Cobb-Douglas: U(x,y)=x^a* y^b":
                                     a = st.slider("Expoente do bem X (a)", 0.1, 1.0, 0.5, 0.01)
                                     b = st.slider("Expoente do bem Y (b)", 0.1, 1.0, 0.5, 0.01)
-                                    U = st.slider("Nível de Utilidade (U)", 1, 10000, 25, 5)
+                                    U = st.slider("Nível de Utilidade (U)", 1, 2000, 25, 1)
                                     st.session_state.acb = a
                                     st.session_state.bcb = b
                                     st.session_state.ucb = U
